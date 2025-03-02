464aea6514f81f0372c466b007e5b43f1f0dfbf4
서성우 <128414393+sungwoo7180@users.noreply.github.com>
2025-03-02 22:27:49 +0900

    Delete src directory

--- src/main/java/com/example/chatserver/config/constant/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.example.chatserver.config.constant;
-
-public enum UserRole {
-    SUPER_ADMIN,    // 최고 관리자 - 모든 유저 CRUD 권한 보유, 중관 관리자 관리
-    ADMIN,          // 중관 관리자 - 채팅방 메세지 신고 내역 확인 및 제재
-    USER            // 일반 사용자
-}
--- src/main/java/com/example/chatserver/config/constant/UserStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatserver.config.constant;
-
-public enum UserStatus {
-    ACTIVE,         // 활성 계정
-    SUSPENDED,      // 정지된 계정
-    BANNED,         // 영구 정지 계정
-    DELETED,        // 삭제된 계정
-    DELETING        // 삭제 진행중
-}
