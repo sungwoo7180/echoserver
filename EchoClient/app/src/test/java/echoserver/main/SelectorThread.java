225da76fa578d0703ea050b5f6927ee4b08bf31a
sungwoo7180 <sungwoo7180@hanmail.net>
2025-04-07 04:41:07 +0900

    Main Thread 에서 Selector 처리를 별도의 Thread 에서 처리하도록 분리.

---
+++ EchoClient/app/src/test/java/echoserver/main/SelectorThread.java
@@ -0,0 +1,46 @@
+// SelectorThread.java
+package echoserver.main;
+
+import java.io.IOException;
+import java.nio.channels.Selector;
+import java.nio.channels.SelectionKey;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+
+public class SelectorThread implements Runnable {
+    private final Selector selector;
+    private final ExecutorService workerPool;
+
+    public SelectorThread(Selector selector, ExecutorService workerPool) {
+        this.selector = selector;
+        this.workerPool = workerPool;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("[SelectorThread] 시작됨");
+        while (true) {
+            try {
+                selector.select();
+                Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+                while (iter.hasNext()) {
+                    SelectionKey key = iter.next();
+                    iter.remove();
+
+                    if (!key.isValid()) {
+                        key.cancel();
+                        continue;
+                    }
+
+                    if (key.isAcceptable()) {
+                        ChatServer2.handleAccept(key, selector);
+                    } else if (key.isReadable()) {
+                        workerPool.submit(() -> ChatServer2.handleReadWrite(key));
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("[SelectorThread] 에러: " + e.getMessage());
+            }
+        }
+    }
+}
